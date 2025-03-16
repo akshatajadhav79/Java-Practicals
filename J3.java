093d5b76e187e962eb958c98d4dda9fa0321feba
akshata <akshatajadhav7902@gmail.com>
2025-03-17 00:40:59 +0530

    journal is done

--- J3.java
+++ J3.java
@@ -1,66 +1,61 @@
-// import java.util.Scanner;
 
-// class ZeroNumberException extends Exception {
-//     public ZeroNumberException(String message) {
-//         super(message);
-//     }
-// }
+import java.util.*;
 
-// class J3 {
-//     public static boolean isPrime(int number) {
-//         if (number < 2) {
-//             return false; // 0 and 1 are not prime numbers
-//         }
-//         for (int i = 2; i <= Math.sqrt(number); i++) {
-//             if (number % i == 0) {
-//                 return false; // Found a divisor, not prime
-//             }
-//         }
-//         return true; // No divisors found, it's prime
-//     }
+class InvalidDateException extends Exception {
 
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         try {
-//             System.out.print("Enter the number: ");
-//             int n = sc.nextInt();
-//             if (n >= 0) {
-//                 throw new ZeroNumberException("Number is not positive");
-//             } else {
-//                 if (isPrime(n)) {
-//                     System.out.println(n + " is a Prime Number");
-//                 } else {
-//                     System.out.println(n + " is not a Prime Number");
-//                 }
-//             }
-//         } catch (ZeroNumberException e) {
-//             System.out.println("Error: " + e.getMessage());
-//         } catch (Exception e) {
-//             System.out.println("Invalid input! Please enter an integer.");
-//         } finally {
-//             sc.close(); // Closing the scanner to prevent resource leak
-//         }
-//     }
-// }
+    public InvalidDateException(String message) {
+        super(message);
+    }
+}
 
-import java.util.ArrayList;
-import java.util.Iterator;
-class J3{
+class Date {
+
+    private int day, month, year;
+
+    public Date(int day, int month, int year) throws InvalidDateException {
+        if (!isValidDate(day, month, year)) {
+            throw new InvalidDateException("Invalid Date:" + day + "/" + month + "/" + year);
+        }
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    private boolean isValidDate(int day, int month, int year) {
+        if (month < 1 || month > 12) {
+            return false;
+        }
+        int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (month == 2 && isLeapYear(year)) {
+            daysInMonth[2] = 29;
+        }
+        return day >= 1 && day <= daysInMonth[month];
+    }
+
+    private boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
+    public void display() {
+        System.out.println("Valid Date: " + day + "/" + month + "/" + year);
+    }
+}
+
+class J3 {
+
     public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        a.add(12);
-        a.add(13);
-        a.add(14);
-        a.add(15);
-        a.add(16);
-        a.add(17);
-
-        System.out.println("Array="+a);
-
-        Iterator<Integer> i = a.iterator(a.size());
-        while (i.hasPrevious()){
-                System.out.println("Element is="+i.Previous());
-
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter day: ");
+            int day = sc.nextInt();
+            System.out.print("Enter month: ");
+            int month = sc.nextInt();
+            System.out.print("Enter year: ");
+            int year = sc.nextInt();
+            Date date = new Date(day, month, year);
+            date.display();
+        } catch (InvalidDateException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
